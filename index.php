037463028d3dd833778a9c2bbdab981e5bab2576
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-01-24 22:01:05 +0700

    Continue with PHP Training You Tube Course PHP Course for Beginners by Dani Krossing
    Completed the login and signup system

--- index.php
+++ index.php
@@ -1,39 +1,24 @@
-<?php
-require_once 'includes/config_session.inc.php';
-require_once 'includes/signup_view.inc.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="index.css">
+  <meta name="viewport" content="width=\, initial-scale=1.0">
   <title>Document</title>
 </head>
 
 <body>
-  <h3>Login</h3>
-
-  <form action="includes/login.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <button>Login</button>
-  </form>
-
-  <h3>Signup</h3>
-
-  <form action="includes/signup.inc.php" method="post">
-    <?php
-    signup_inputs();
-    ?>
-
-    <button>Signup</button>
-  </form>
 
   <?php
-  check_signup_errors();
+  $brand = "Volvo";
+  $color = "Green";
+
+  function getCarInfo($brand, $color)
+  {
+    return "Brand: " . $brand . ", Color: " . $color;
+  }
+
+  echo getCarInfo($brand, $color);
   ?>
 </body>
 
